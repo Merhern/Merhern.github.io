b5987919db3cff04dbe26ad46eabcd288d67a8eb
Merhern <runuk305@seznam.cz>
2020-10-14 16:42:43 +0200

    JS; question edit and intro text; better css

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function hide() {
+  var f = document.getElementById("form1");
+  if (f.style.display === "none") {
+    f.style.display = "block";
+  } else {
+    f.style.display = "none";
+  }
+}
