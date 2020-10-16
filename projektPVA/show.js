d381bca99d22bf6281ddbe4248b671abdadea7e7
Merhern <runuk305@seznam.cz>
2020-10-16 15:14:20 +0200

    v24

--- projektPVA/show.js
+++ projektPVA/show.js
@@ -2,12 +2,15 @@ let article = document.createElement("article");
 article.setAttribute("id","12345")
 let nazev = document.createElement("h2");
 nazev.innerHTML = "Test1";
-document.getElementById("12345").appendChild(nazev);
+//document.getElementById("12345").appendChild(nazev);
 
-/*let article2 = document.createElement("article");
+let article2 = document.createElement("article");
 article2.setAttribute("id","12346")
 let nazev2 = document.createElement("h2");
 nazev2.innerHTML = "Test1";
-document.getElementById("12346").appendChild(nazev2);*/
+//document.getElementById("12346").appendChild(nazev2);
 
 document.body.appendChild(article);
+document.body.appendChild(nazev);
+document.body.appendChild(article2);
+document.body.appendChild(nazev2);
