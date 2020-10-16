4f60c560dd72bdc4b3b8b54f16c32586c39a853e
Merhern <runuk305@seznam.cz>
2020-10-16 14:04:18 +0200

    v9

--- projektPVA/show.js
+++ projektPVA/show.js
@@ -3,9 +3,3 @@ article.id = "12345";
 var nazev = document.createElement("h2");
 nazev.innerHTML = "Test1";
 document.getElementById("12345").appendChild(nazev);
-
-var article2 = document.createElement("article");
-article.id = "123456";
-var nazev2 = document.createElement("h2");
-nazev.innerHTML = "Test2";
-document.getElementById("123456").appendChild(nazev2);
