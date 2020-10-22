e625ed1eddee01db1bc1e45bb9ee31d2883bcfe4
Merhern <runuk305@seznam.cz>
2020-10-22 18:09:26 +0200

    php fix 2; connect to database

--- send.php
+++ send.php
@@ -1,39 +1,43 @@
 <?php
-    $_POST["one"] = $one;
-    $_POST["one"] = $two;
-    $_POST["one"] = $three;
-    $_POST["one"] = $four;
-    $_POST["one"] = $five;
-    $_POST["one"] = $six;
-    $_POST["one"] = $seven;
-    $_POST["one"] = $eight;
-    $_POST["one"] = $nine;
-    $_POST["one"] = $ten;
-    $_POST["one"] = $eleven;
-    $_POST["one"] = $twelve;
-    $_POST["one"] = $thirteen;
-    $_POST["one"] = $fourteen;
-    $_POST["one"] = $fifteen;
-    $_POST["one"] = $sixteen;
-    $_POST["one"] = $gender;
-    $_POST["one"] = $age;
-    $_POST["one"] = $SES;
-    $_POST["one"] = $religion;
-    $_POST["one"] = $race;
-    $_POST["one"] = $social;
-    $_POST["one"] = $economy;
-    $_POST["one"] = $kids;
-    $_POST["one"] = $meat;
-    $SDO_Score = $one + $two + $three + $four + $five + $six + $seven + $eight + $nine + $ten + $eleven + $twelve + $thirteen + $fourteen + $fifteen + $sixteen;
+  $servername = "localhost";
+  $username = "serf";
+  $password = "serf12345";
 
-    if (($one <> "") && ($two <> "") && ($three <> "") && ($four <> "") && ($five <> "") && ($six <> "") && ($seven <> "") && ($eight <> "") &&
-          ($nine <> "") && ($ten <> "") && ($eleven <> "") && ($twelve <> "") && ($thirteen <> "") && ($fourteen <> "") && ($fifteen <> "") && ($sixteen <> "") &&
-          ($gender <> "") && ($age <> "") && ($SES <> "") && ($religion <> "") && ($race <> "") && ($social <> "") && ($economy <> "") && ($kids <> "") &&
-          ($meat <> "")) {
-            echo $SDO_Score;
-            echo "Done";
+  // Create connection
+  $conn = new mysqli($servername, $username, $password);
 
-        }
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  echo "Connected successfully";
 
 
+  $_POST["one"] = $one;
+  $_POST["one"] = $two;
+  $_POST["one"] = $three;
+  $_POST["one"] = $four;
+  $_POST["one"] = $five;
+  $_POST["one"] = $six;
+  $_POST["one"] = $seven;
+  $_POST["one"] = $eight;
+  $_POST["one"] = $nine;
+  $_POST["one"] = $ten;
+  $_POST["one"] = $eleven;
+  $_POST["one"] = $twelve;
+  $_POST["one"] = $thirteen;
+  $_POST["one"] = $fourteen;
+  $_POST["one"] = $fifteen;
+  $_POST["one"] = $sixteen;
+  $_POST["one"] = $gender;
+  $_POST["one"] = $age;
+  $_POST["one"] = $SES;
+  $_POST["one"] = $religion;
+  $_POST["one"] = $race;
+  $_POST["one"] = $social;
+  $_POST["one"] = $economy;
+  $_POST["one"] = $kids;
+  $_POST["one"] = $meat;
+  $SDO_Score = $one + $two + $three + $four + $five + $six + $seven + $eight + $nine + $ten + $eleven + $twelve + $thirteen + $fourteen + $fifteen + $sixteen 
+  if (($one <> "") && ($two <> "") && ($three <> "") && ($four <> "") && ($five <> "") && ($six <> "") && ($seven <> "") && ($eight <> "") &&       ($nine <> "") && ($ten <> "") && ($eleven <> "") && ($twelve <> "") && ($thirteen <> "") && ($fourteen <> "") && ($fifteen <> "") && ($sixteen <> "") &&        ($gender <> "") && ($age <> "") && ($SES <> "") && ($religion <> "") && ($race <> "") && ($social <> "") && ($economy <> "") && ($kids <> "") &&        ($meat <> "")) {          echo $SDO_Score;          echo "Done";      }
 ?>
