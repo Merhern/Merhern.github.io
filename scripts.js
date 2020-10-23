32734bd55a0b19b5992958592def7ec49ccf308e
Merhern <runuk305@seznam.cz>
2020-10-23 18:22:01 +0200

    calculate probability and gender specific comparison

--- scripts.js
+++ scripts.js
@@ -4,8 +4,52 @@ function move() {
 }
 
 function cookie() {
-  var x =  document.cookie.split("=")[1];
-  var y = x / 1.90714286;
-  document.getElementById("sdo").innerHTML = "Your Social Dominance Orientation is:" + x;
-  document.getElementById("compared").innerHTML = "That is " + y + "x more than average US citizen [1]";
+  var x =  parseFloat (document.cookie.split("=")[1]);
+  var y = x / 1.89404761904761904762;
+  var zz = (x - 1.89404761904761904762) / 0.85238095238095238095;
+  var ndp;
+  var m = x / 2.04642857142857142857;
+  var f = x / 1.77261904761904761905;
+
+  function GetZPercent(z) {
+
+    // z == number of standard deviations from the mean
+
+    // if z is greater than 6.5 standard deviations from the mean the
+    // number of significant digits will be outside of a reasonable range
+
+    if (z < -6.5) {
+      return 0.0;
+    }
+
+    if (z > 6.5) {
+      return 1.0;
+    }
+
+    var factK = 1;
+    var sum = 0;
+    var term = 1;
+    var k = 0;
+    var loopStop = Math.exp(-23);
+
+    while(Math.abs(term) > loopStop) {
+      term = .3989422804 * Math.pow(-1,k) * Math.pow(z,k) / (2 * k + 1) / Math.pow(2,k) * Math.pow(z,k+1) / factK;
+      sum += term;
+      k++;
+      factK *= k;
+    }
+
+    sum += 0.5;
+
+    return sum;
+  }
+
+  document.getElementById("sdo").innerHTML = "Your Social Dominance Orientation is: " + x.toFixed(2);
+
+  document.getElementById("compared").innerHTML = "That is " + y.toFixed(2) + "x more than average US citizen (~ 1.89)";
+  document.getElementById("nd").innerHTML = "You score higher than "+ GetZPercent(zz) +"% of people in the US";
+  document.getElementById("female").innerHTML = "That is " + f.toFixed(2) + "x more than average US female (~ 1.77)";
+  document.getElementById("male").innerHTML = "That is " + m.toFixed(2) + "x more than average US male (~ 2.05)";
+
+
 }
