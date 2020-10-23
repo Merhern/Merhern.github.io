21260a4ae8dc2c3cfa9d01e4512aeba7a42bde95
Merhern <runuk305@seznam.cz>
2020-10-23 19:52:39 +0200

    added more usefull information concernig sex; hereditability info

--- scripts.js
+++ scripts.js
@@ -7,8 +7,8 @@ function cookie() {
   var x =  parseFloat (document.cookie.split("=")[1]);
   var y = x / 1.90952380952380952381;
   var z_score = (x - 1.90952380952380952381) / 0.85238095238095238095;
-  var m = x / 2.04642857142857142857;
-  var f = x / 1.77261904761904761905;
+  var m_score = (x - 2.04642857142857142857) / 0.85238095238095238095;
+  var f_score = (x - 1.77261904761904761905) / 0.85238095238095238095;
 
   console.log(z_score);
 
@@ -47,9 +47,9 @@ function cookie() {
   document.getElementById("sdo").innerHTML = "Your Social Dominance Orientation is: " + x.toFixed(2);
 
   document.getElementById("compared").innerHTML = "That is " + y.toFixed(2) + "x more than average US citizen. (~ 1.91)";
-  document.getElementById("nd").innerHTML = "You score higher than of " + GetZPercent(z_score).toFixed(2) + "% people in the US.";
-  document.getElementById("female").innerHTML = "That is " + f.toFixed(2) + "x more than average US female. (~ 1.77)";
-  document.getElementById("male").innerHTML = "That is " + m.toFixed(2) + "x more than average US male. (~ 2.05)";
+  document.getElementById("nd").innerHTML = "You score higher than " + GetZPercent(z_score).toFixed(2) + "% of people in the US.";
+  document.getElementById("female").innerHTML = "You score higher than " + GetZPercent(f_score).toFixed(2) + "% of women in the US.";
+  document.getElementById("male").innerHTML = "You score higher than " + GetZPercent(m_score).toFixed(2) + "% of men in the US.";
 
 
 }
